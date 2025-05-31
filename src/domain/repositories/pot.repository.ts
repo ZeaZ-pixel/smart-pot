418cceae769aa2904f8d86108c8ffe4ccecd7c7c
Nariman <onlanovnar@gmail.com>
2025-05-31 14:35:39 +0500

    Add pot repository

--- src/domain/repositories/pot.repository.ts
+++ src/domain/repositories/pot.repository.ts
@@ -2,6 +2,7 @@ import { PotEntity } from '../entities/pot.entity';
 
 export interface IPotRepository {
   save(pot: PotEntity): Promise<PotEntity>;
-  delete(pot_id: number): Promise<void | null>;
-  edit(pot: PotEntity): Promise<PotEntity>;
+  deleteById(potId: number): Promise<void | null>;
+  findById(potId: number): Promise<PotEntity | null>;
+  getAll(limit: number, offset: number): Promise<PotEntity[]>;
 }
